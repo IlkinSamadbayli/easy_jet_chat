a607aa8764c871ec1bd4baa6bf7de712e7b66be8
Hello Haystack <Hello Haystack@DESKTOP-K13HK9J>
2023-04-10 14:07:35 +0400

    Adding shared preference

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:easy_jet_chat/home_page.dart';
+import 'package:easy_jet_chat/helper/helper_function.dart';
+import 'package:easy_jet_chat/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'pages/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,8 +11,30 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isLoggedIn = false;
+
+  @override
+  void initState() {
+    getuserLoggedInStatus();
+    super.initState();
+  }
+
+  getuserLoggedInStatus() async {
+    await HelperFunction.getLoggedInStatus().then((value) => {
+          if (value != null)
+            {
+              _isLoggedIn = value,
+            }
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +42,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        
       ),
-      home: const HomePAge(),
+      debugShowCheckedModeBanner: false,
+      home: _isLoggedIn? const HomePage() : const LoginPage(),
     );
   }
 }
